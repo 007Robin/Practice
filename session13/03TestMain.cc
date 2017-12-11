e719b9b2a2a27634764378ab1d0065bbb69fdcc8
007Robin <30958079+007Robin@users.noreply.github.com>
2017-12-11 13:42:21 -0500

    Add files via upload

---
+++ session13/03TestMain.cc
@@ -0,0 +1,25 @@
+#include "03LinkedList.hh"
+#include <string>
+using namespace std;
+#include "03LinkedList.cc"
+
+//void f();
+
+int main() {
+	LinkedList<int> a; // empty
+	a.addFirst(3); 
+	a.addFirst(1);
+	a.addFirst(4); // 4 1 3
+	a.addLast(1); // 4 1 3 1
+	a.removeFirst(); // 1 3 1
+	a[1] = 19;
+	cout << a << '\n';
+//	f();
+	LinkedList<int> c = a; // call copy constructor
+	cout << c << '\n';
+
+	LinkedList<string> b;
+	b.addFirst("hello");
+	b.addLast("goodbye");
+	cout << b << '\n';
+}
