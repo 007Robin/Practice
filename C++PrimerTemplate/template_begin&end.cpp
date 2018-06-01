5c1686bcb458a03e6936241bf83986fe0c9b1578
007Robin <30958079+007Robin@users.noreply.github.com>
2018-06-01 17:32:18 -0400

    Update and rename 16_6.cpp to template_begin&end.cpp

--- C++PrimerTemplate/template_begin&end.cpp
+++ C++PrimerTemplate/template_begin&end.cpp
@@ -3,6 +3,9 @@
 #include<string>
 
 using namespace std;
+//T is type parameter
+//N is nontype parameter, a value. It can be int, pointer, rvalue reference... 
+//passing N must be a const value, like 4 in v[4], 5 in s[5] <--> const expression
 
 template<typename T, unsigned N>
 T* start(T (&v)[N]){
@@ -17,7 +20,9 @@ int main(){
 	int v[4] = {9,5,1,9};
 	cout << *start(v) << " ";
 	cout << *last(v) << " ";
-	cout << endl;
+	
+	cout << '\n';
+	
 	double s[5] = {9.01, 5.01, 1.01, 2.011, 6.7};
 	cout << *start(s) << " ";
 	cout << *last(s) << " ";
