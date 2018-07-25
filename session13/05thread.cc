322a9275ee515f4a056ca46ff55a591888db8aac
007Robin <30958079+007Robin@users.noreply.github.com>
2018-07-24 22:06:32 -0400

    Update 05thread.cc

--- session13/05thread.cc
+++ session13/05thread.cc
@@ -32,4 +32,6 @@ int main() {
 flush endl 都是刷新了缓冲区输出, 如果不加就会只往buffer里写东西但没有输出。
 join 是两个线程一个完了以后 再执行另一个
 mutex 是锁。使用的话就是mutex.lock()  mutex.unlock()
-   */
+  
+result: hello bye 0 hello1 bye hello2 hello3 bye  hello4 hello5 bye  hello6 hello7 bye  hello8 hello9 bye  hello10 hello11 bye  hello12c hello13 bye 
+  */
